1049e3dad31f0d49da23a6e943a045379f95c6ca
jpuri <jyotipuri@gmail.com>
2021-11-13 06:00:15 +0530

    Adding network status section on edit gas fee modal

---
+++ ui/components/app/edit-gas-fee-popover/network-status/status-slider/status-slider.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const GradientColors = [
+  '#037DD6',
+  '#1876C8',
+  '#2D70BA',
+  '#4369AB',
+  '#57629E',
+  '#6A5D92',
+  '#805683',
+  '#9A4D71',
+  '#B44561',
+  '#C54055',
+];
+
+const StatusSlider = () => {
+  const statusValue = 0.5;
+  const sliderValueNumeric = Math.round(statusValue * 10);
+  return (
+    <div className="status-slider">
+      <div
+        className="status-slider__arrow"
+        style={{
+          borderTopColor: GradientColors[sliderValueNumeric],
+          marginLeft: `${sliderValueNumeric * 10 - 10}%`,
+        }}
+      />
+      <div className="status-slider__line" />
+      <div
+        className="status-slider__label"
+        style={{ color: GradientColors[sliderValueNumeric] }}
+      >
+        {statusValue <= 0.5 ? 'Stable' : 'Unstable'}
+      </div>
+    </div>
+  );
+};
+
+export default StatusSlider;
